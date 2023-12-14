795900ea1a8201d4ee4abf7b4051ff9d5857ccf6
michelmnds <m1tchofc@gmail.com>
2023-12-14 15:28:12 +0000

    feat: can create, complete and delete a task

--- to-do-app/src/components/SideBar/index.jsx
+++ to-do-app/src/components/SideBar/index.jsx
@@ -1,10 +1,11 @@
 import "./style.css";
+import { Link } from "react-router-dom";
 
 export const SideBar = () => {
   return (
     <div className="sideBarContainer">
-      <a href="">Home</a>
-      <a href="">About</a>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
     </div>
   );
 };
